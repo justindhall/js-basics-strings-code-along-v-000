09a3deb837ae51b358b45c028fa12ab027da6529
Justin Hall <hall.justin.d@gmail.com>
2018-05-11 20:23:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, `;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
 
