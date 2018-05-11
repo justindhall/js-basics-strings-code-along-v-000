7ae7f09cd04ca1a60d657d7708ad60af68d98659
Justin Hall <hall.justin.d@gmail.com>
2018-05-11 20:18:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const currentUser = "Justin Hall";
 
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
 
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!";
+
+const shortGreeting = "Welcome" + currentUser
+
